006a59a54946ba547b583ff53331e7eb11022931
OPerle <oli-perle@t-online.de>
2024-06-17 10:11:07 +0200

    Namen von stores angepasst

--- frontend_streamlit/store.py
+++ frontend_streamlit/store.py
@@ -3,8 +3,8 @@ from enum import Enum
 
 class SessionStateKey(Enum):
     MAP_DATA = 'map_data'
-    STADTTEILE_LEIPZIG = 'stadtteile_leipzig'
-    KRANKENHAUSER_LEIPZIG = 'krankenhauser_leipzig'
+    GEMEINDEN = 'gemeinden'
+    KRANKENHAUSER = 'krankenhauser'
     SCENARIOS = 'scenarios'
 
 class Store():
@@ -16,23 +16,23 @@ class Store():
     def initialize(self):
         st.session_state = {
             SessionStateKey.MAP_DATA.value: {
-                 SessionStateKey.STADTTEILE_LEIPZIG.value: None,
-                 SessionStateKey.KRANKENHAUSER_LEIPZIG.value: None
+                 SessionStateKey.GEMEINDEN.value: None,
+                 SessionStateKey.KRANKENHAUSER.value: None
             },
             SessionStateKey.SCENARIOS.value: []
         }
 
-    def set_stadtteile_leipzig(self, data):
-        st.session_state[SessionStateKey.MAP_DATA.value][SessionStateKey.STADTTEILE_LEIPZIG.value] = data
+    def set_gemeinden(self, data):
+        st.session_state[SessionStateKey.MAP_DATA.value][SessionStateKey.GEMEINDEN.value] = data
 
-    def get_stadtteile_leipzig(self):
-        return st.session_state[SessionStateKey.MAP_DATA.value][SessionStateKey.STADTTEILE_LEIPZIG.value]
+    def get_gemeinden(self):
+        return st.session_state[SessionStateKey.MAP_DATA.value][SessionStateKey.GEMEINDEN.value]
 
-    def set_krankenhauser_leipzig(self, data):
-        st.session_state[SessionStateKey.MAP_DATA.value][SessionStateKey.KRANKENHAUSER_LEIPZIG.value] = data
+    def set_krankenhauser(self, data):
+        st.session_state[SessionStateKey.MAP_DATA.value][SessionStateKey.KRANKENHAUSER.value] = data
 
-    def get_krankenhauser_leipzig(self):
-        return st.session_state[SessionStateKey.MAP_DATA.value][SessionStateKey.KRANKENHAUSER_LEIPZIG.value]
+    def get_krankenhauser(self):
+        return st.session_state[SessionStateKey.MAP_DATA.value][SessionStateKey.KRANKENHAUSER.value]
 
     def get_scenarios(self):
         return st.session_state.get(SessionStateKey.SCENARIOS.value, [])
