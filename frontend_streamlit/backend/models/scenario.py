e9e5be165f7b65905f4cd6766b4a5f753af2ca2c
AbdulbareeHussein <abed-albare@hotmail.com>
2024-06-28 09:52:36 +0200

    AbHu: constructor for scenario class

--- frontend_streamlit/backend/models/scenario.py
+++ frontend_streamlit/backend/models/scenario.py
@@ -7,8 +7,19 @@ from backend.models.region import Region
 
 @dataclass
 class Scenario:
+    """
+       Represents the entire scenario containing regions and hospitals.
+
+       Attributes:
+       - calculation (dict): Contains calculation parameters like radius.
+       - regions (list): List of Region objects.
+       - hospitals (list): List of Hospital objects.
+       """
     regions: list[Region]
     calculation: Calculation
     hospitals: list[Hospital]
 
-
+    def __init__(self):
+        self.calculation = None
+        self.regions = []
+        self.hospitals = []
